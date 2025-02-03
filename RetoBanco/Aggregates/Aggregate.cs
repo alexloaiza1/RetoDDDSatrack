e62afe3168cd60a17d941ca632288ccbe3b65c98
Alexander_Loaiza <Alexander_Loaiza@PRIALEXANDERL>
2025-02-03 15:04:15 -0500

    Agrego estructura inicial proyecto

---
+++ RetoBanco/Aggregates/Aggregate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetoBanco.Domain.Aggregates
+{
+    public abstract class Aggregate<T>(T id, DateTime createdOn, DateTime lastModifiedOn) where T : struct
+    {
+
+        private readonly IList<IDomainEvent> _domainEvents = [];
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public IReadOnlyKist<IDomainEvent> GetDomainEvents()
+        {
+            return [.. _domainEvents];
+
+
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+
+        public T Id { get; private  set; }
+
+        public DateTime CreatedOn { get; private  set; }
+
+        public DateTime LastModifiedOn { get; private set; }
+    }
+
+}
